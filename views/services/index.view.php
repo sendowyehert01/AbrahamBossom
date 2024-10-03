d1a64e5a88f873e851bbede4f068edf85aa0aeac
Sendo <133893305+sendowyehert01@users.noreply.github.com>
2024-10-04 06:06:48 +0800

    Remove tailwind class in index.view.php

--- views/services/index.view.php
+++ views/services/index.view.php
@@ -3,17 +3,16 @@
 <?php require base_path('views/partials/banner.php') ?>
 
   <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <div>
       <ul>
-        <?php /** @var array<string, array> $notes */ ?>
         <?php foreach($services as $service) : ?>
-          <a href="/service-offer?id=<?= $service['id'] ?>" class="text-blue-500 hover:underline">
+          <a href="/service-offer?id=<?= $service['id'] ?>">
             <li><?= htmlspecialchars($service['name']) ?></li>
           </a>
         <?php endforeach; ?>
       </ul>
-      <p class="mt-6">
-        <a href="/services-offer/create" class="text-blue-500 underline">Create Service</a>
+      <p>
+        <a href="/services-offer/create">Create Service</a>
       </p>
     </div>
   </main>
