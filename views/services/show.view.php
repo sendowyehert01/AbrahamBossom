2dd88463fcb51215eee86ed33df1c62d6e311264
Sendo <133893305+sendowyehert01@users.noreply.github.com>
2024-10-04 06:08:35 +0800

    Remove tailwind class in show.view.php

--- views/services/show.view.php
+++ views/services/show.view.php
@@ -3,14 +3,14 @@
 <?php require base_path('views/partials/banner.php') ?>
 
   <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <p class="mb-6">
-        <a href="/services-offer" class="text-blue-500 underline">Go Back</a>
+    <div>
+      <p>
+        <a href="/services-offer">Go Back</a>
       </p>
           <p><?= $service['name'] ?></p>
           <p><?= $service['description'] ?></p>
 
-          <footer class="mt-6">
+          <footer>
             <a href="/services-offer/edit?id=<?= $service['id'] ?>">Edit Service</a>
           </footer>
 
